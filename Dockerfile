e5e0fef27077bb31cfc6a9ee7fa3e81af7a7d37b
Augusto <dev.augusto@gmail.com>
2019-01-08 21:39:15 -0200

    - Adjust the files to receive minimum docker image

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Build runtime image
-FROM microsoft/dotnet:aspnetcore-runtime
+FROM microsoft/dotnet:2.2-aspnetcore-runtime
 WORKDIR /app
-COPY ./src/API/bin/Release/netcoreapp2.1/publish/ .
+COPY /src/API/bin/Debug/netcoreapp2.1/publish/ .
 ENTRYPOINT ["dotnet", "Imobilizados.API.dll"]
--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -48,9 +48,6 @@ namespace Imobilizados.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
